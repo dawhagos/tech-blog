961ab6ef47132de21964e07365692930541177f4
Dawit Hagos <dawhagos@gmail.com>
2023-02-21 23:51:15 -0800

    created UI with most of the pages, started express api

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const { config } = require("dotenv");
+config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(MONGO_URL).then(() => {
+  console.log("listening on port 4000");
+  app.listen(4000);
+});
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,8 +1,28 @@
 import {Link} from "react-router-dom";
-import {userContext, userEffect, useState} from "react";
+import {useContext, useEffect} from "react";
+import {UserContext} from "./UserContext";
 
 export default function Header() {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
 
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
 
   return (
     <header>
@@ -23,5 +43,4 @@ export default function Header() {
       </nav>
     </header>
   );
-  
 }
