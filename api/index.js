c8c3c1b1917ad78b554a7232bb4d4efcb3b8ed99
Dawit Hagos <dawhagos@gmail.com>
2023-03-18 14:09:01 -0700

    bug fix

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ async function connectToDatabase() {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
   } catch (error) {
