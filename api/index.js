e4c3bc79aacd10a8721a2d52f10bcc75191770bb
Dawit Hagos <dawhagos@gmail.com>
2023-03-18 13:22:23 -0700

    fixed async db connection

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ async function connectToDatabase() {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
   } catch (error) {
