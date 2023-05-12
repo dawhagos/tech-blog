8d803ce9494f6b28739cb35dfe63d8a53ae1f11f
Dawit Hagos <dawhagos@gmail.com>
2023-05-12 01:31:26 -0700

    added loading spinner on index page

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,20 +1,29 @@
 import Post from "../Post";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
-  const [posts,setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_APP_API_URL}/post`).then(response => {
-      response.json().then(posts => {
+    fetch(`${import.meta.env.VITE_APP_API_URL}/post`)
+      .then((response) => response.json())
+      .then((posts) => {
         setPosts(posts);
+        setIsLoading(false);
       });
-    });
   }, []);
   return (
     <>
-      {posts.length > 0 && posts.map(post => (
-        <Post key={post._id} {...post} />
-      ))}
+      {isLoading ? (
+        <div className="spinner-container">
+          <div className="loading-spinner"></div>
+        </div>
+      ) : (
+        <>
+          {posts.length > 0 &&
+            posts.map((post) => <Post key={post._id} {...post} />)}
+        </>
+      )}
     </>
   );
 }
